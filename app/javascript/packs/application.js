a9204a9a6fee935a7b2901b2235da4ff8f98d53f
hasan1434 <hafzal794@gmail.com>
2022-02-20 08:10:04 +0500

    worked on select2 heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,25 +10,18 @@ require("jquery")
 
 
 import $ from 'jquery'
-import 'select2/dist/css/select2.css'
+// import 'select2/dist/css/select2.css'
 import 'select2'
 /// if you don't have turnbolinks then use this:
 //// document.addEventListener('DOMContentLoaded', () => { 
 window.addEventListener('turbolinks:load', () => {
-  $('.select2').select2({
-    tags: true,
-    tokenSeparators: [',', ' ']
-  });
+    $('.select2').select2({
+        tags: true,
+        tokenSeparators: [',', ' ']
+    });
 })
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-// $(document).ready(function() {
-//     console.log("ready!");
-//     $( ".dropdown" ).select2({
-//         theme: "bootstrap"
-//     });
-// });
