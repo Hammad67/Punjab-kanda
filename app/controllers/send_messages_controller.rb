dfe9f34a4e0b11c68be3ff49b85dd50fc4e88369
hasan1434 <hafzal794@gmail.com>
2022-02-20 12:56:07 +0500

    done

--- app/controllers/send_messages_controller.rb
+++ app/controllers/send_messages_controller.rb
@@ -13,7 +13,9 @@ class SendMessagesController < ApplicationController
   end
 
   def create
-    @messages_errors=[]
+    @not_send_messages_phone_numbers = []
+    @not_send_messages_response = []
+    @not_send_messages_ids = []
     @send_message = SendMessage.new(send_message_params)
     @send_message.valid?
     if @send_message.errors.present?
@@ -39,6 +41,16 @@ class SendMessagesController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @send_message.errors, status: :unprocessable_entity }
       end
+    elsif @not_send_messages_ids.present?
+      @not_send_messages_ids.each do |send_message_id|
+        send_message = SendMessage.find_by(id: send_message_id)
+        send_message.destroy if send_message.present?
+      end
+      @send_message = SendMessage.new(send_message_params)
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @send_message.errors, status: :unprocessable_entity }
+      end
     else
       redirect_to root_path, notice: "Message Send Succesfully"
     end
@@ -50,23 +62,64 @@ class SendMessagesController < ApplicationController
       format.json { head :no_content }
     end
   end
+
   def save_messages_party(party_id)
     @send_message = SendMessage.new(send_message_params)
     @send_message.party_id = party_id
     @send_message.phone_number = nil
     @send_message.save
+    find_number(@send_message)
   end
+
   def save_messages_driver(driver_id)
     @send_message = SendMessage.new(send_message_params)
     @send_message.driver_id = driver_id
     @send_message.phone_number = nil
     @send_message.save
+    find_number(@send_message)
   end
+
   def save_messages_phone_number(phone_number)
     @send_message = SendMessage.new(send_message_params)
     @send_message.save
+    find_number(@send_message)
   end
+
+  def find_number(send_message)
+    if send_message.party_id.present?
+     send_message_to_number(send_message, send_message.party.phone_number)
+    elsif send_message.driver_id.present?
+     send_message_to_number(send_message, send_message.driver.phone_number)
+   elsif send_message.phone_number.present?
+     send_message_to_number(send_message, send_message.phone_number)
+    end
+ end
+
+ def send_message_to_number(send_message, phone_number)
+   message="
+   Vehicle number: #{send_message.vehicle_number}
+   Product: #{send_message.product}
+   First Load Weight:  #{send_message.before_load_weight}
+   Second Load Weight:  #{send_message.after_load_weight}
+   Net Load Weight:  #{send_message.net_weight}
+   Total Amoount:  #{send_message.cash}"
+   api_key = ENV["API_KEY"]
+   brand_name = ENV["BRAND_NAME"]
+   response= Faraday.post("https://sendpk.com/api/sms.php?api_key=#{api_key}&sender=#{brand_name}&mobile=#{phone_number}&message=#{message}")
+  #  change response.body != "OK"  when get account details
+   if response.body != "OK" 
+     @not_send_messages_phone_numbers << phone_number
+     @not_send_messages_response << response.body
+     @not_send_messages_ids << send_message.id
+   end
+ end
+
+
   private
+  
+  def set_send_message
+    @send_message = SendMessage.find_by(id: params[:id])
+  end
   def send_message_params
     params.require(:send_message).permit(:srial_number, :vehicle_number, :product, :weight_type, :before_load_weight, :after_load_weight, :net_weight, :cash, :phone_number)
   end
--- app/models/goodness_validator.rb
+++ app/models/goodness_validator.rb
@@ -1,7 +1,7 @@
 class GoodnessValidator < ActiveModel::Validator
     def validate(record)
         if !record.phone_number.to_i.between?(923000000000, 923999999999) 
-            record.errors.add :phone_number, "Phone numbers must be in 923026211613 format."
+            record.errors.add :phone_number, "must be in 923026211613 format."
         end
     end
 end
